1949987d10ad61cfea608cf1e5ccda36d59a7273
one-big-unicorn <lwangx26@gmail.com>
2025-01-11 14:46:44 -0500

    image upload

--- server.py
+++ server.py
@@ -1,26 +1,63 @@
 import os
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flask_cors import CORS
+import openai
 
 app = Flask(__name__,
                   static_url_path="",
                   static_folder="web/static",
-                  template_folder="web/templates")
+                  template_folder="web/templates",
+            )
 
+# /
+# /prescriptions
+
+# uploading images to backend
+UPLOAD_FOLDER = "web/uploads"
+@app.route("/upload-photo", methods=["POST"])
+def upload_file():
+    image = request.files.get("image", "")
+    if image:
+        print("File uploaded successfully")
+    else:
+        return
+    
+    # return render_template("upload.html")
+    return ""
+
+# rendering html
 @app.route("/", methods=["GET"])
-
 def getTemplate():
-    print("Rendering template with title and message")
     return render_template('template.html', title="Templates Page", message="Welcome to the Templates Page!")
 
+@app.route("/", methods=["GET"])
+def sendPrescription():
+    pass
+
+prescription = {
+    "#": 0, # number ID
+    "Name": "",  # name of drug
+    "Strength": 0,  # mg/pill
+    "Start Date": "", # start date (in format YYYYMMDD)
+    "Directions": "",  # just a string of directions
+    "Time": 0, # best time in hour from 0-24 to take this medication
+    "Interval": "",  # how often it should be taken in days (minimum 1)
+    "Quantity": "",  # pills in bottle
+    "Refills": "",  # number of refills
+    "End Date": "",  # refill date if there are refills, end date if refills == 0 (in format YYYYMMDD)
+    "Warnings": ""  # any warnings
+}
 
 
+
+
+
+# run
 def create_app():
     app.config['CORS_HEADERS'] = 'Content-Type'
     CORS(app)
     return app
-
 if __name__ == "__main__":
     app = create_app()
     app.run(host="0.0.0.0", port=8080, debug=True)
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -4,9 +4,10 @@ main(() => {
     console.log("hello world")
     const uploadBtn = document.getElementById("avatar")
     uploadBtn.addEventListener("click", async (e) => {
-        const url = "http://10.70.9.185:8080/upload-photo"
+        const url = "http://localhost:8080/upload-photo"
         const formData = new FormData()
         formData.append("image", document.getElementById("avatar").files[0])
+        console.log(document.getElementById("avatar").files[0])
 
         const req = await fetch(url, {
           method: "POST",
