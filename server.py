8926ab695552f9f2cc77385db31d5eeaa0d320d5
one-big-unicorn <lwangx26@gmail.com>
2025-01-11 15:40:18 -0500

    second page

--- server.py
+++ server.py
@@ -1,59 +1,32 @@
-import os
 from flask import Flask
 from flask import render_template, request
 from flask_cors import CORS
-import openai
 
-app = Flask(__name__,
-                  static_url_path="",
-                  static_folder="web/static",
-                  template_folder="web/templates",
-            )
+app = Flask(
+    __name__,
+    static_url_path="",
+    static_folder="web/static",
+    template_folder="web/templates",
+)
 
-# /
-# /prescriptions
-
-# uploading images to backend
-UPLOAD_FOLDER = "web/uploads"
 @app.route("/upload-photo", methods=["POST"])
-def upload_file():
+def process_photo():
     image = request.files.get("image", "")
-    if image:
-        print("File uploaded successfully")
-    else:
-        return
+    image.save("./test.jpg")
     
-    # return render_template("upload.html")
-    return ""
 
-# rendering html
-@app.route("/", methods=["GET"])
-def getTemplate():
-    return render_template('template.html', title="Templates Page", message="Welcome to the Templates Page!")
+
+
+    return 
+    
 
 @app.route("/", methods=["GET"])
-def sendPrescription():
-    pass
+def getTemplate1():
+    return render_template('template-1.html')
+@app.route("/app", methods=["GET"])
+def getTemplate2():
+    return render_template('template-2.html')
 
-prescription = {
-    "#": 0, # number ID
-    "Name": "",  # name of drug
-    "Strength": 0,  # mg/pill
-    "Start Date": "", # start date (in format YYYYMMDD)
-    "Directions": "",  # just a string of directions
-    "Time": 0, # best time in hour from 0-24 to take this medication
-    "Interval": "",  # how often it should be taken in days (minimum 1)
-    "Quantity": "",  # pills in bottle
-    "Refills": "",  # number of refills
-    "End Date": "",  # refill date if there are refills, end date if refills == 0 (in format YYYYMMDD)
-    "Warnings": ""  # any warnings
-}
-
-
-
-
-
-# run
 def create_app():
     app.config['CORS_HEADERS'] = 'Content-Type'
     CORS(app)
