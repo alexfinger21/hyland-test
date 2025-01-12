66a49fcd615b82d58c8781918878cce8d54b8d22
alexfinger21 <cdx9933@gmail.com>
2025-01-11 22:51:53 -0500

    upload #2

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -5,8 +5,8 @@ main(() => {
     uploadBtn.addEventListener("click", async (e) => {
         const url = "http://localhost:8080/upload-photo"
         const formData = new FormData()
-        formData.append("image", document.getElementById("avatar").files[0])
-        console.log(document.getElementById("avatar").files[0])
+        formData.append("image", document.getElementById("files").files[0])
+        console.log(document.getElementById("files").files[0])
 
         const req = await fetch(url, {
           method: "POST",
